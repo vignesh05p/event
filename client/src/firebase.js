941c305aaa8a0adb4da4d0d3f880994f486daf57
Vignesh prabhu <138098975+vignesh05p@users.noreply.github.com>
2025-02-08 12:58:33 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,15 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCQW4GPUEN3-u6jVmWBAcSF7lpqr7YN0DA",
-    authDomain: "eventmanagenment.firebaseapp.com",
-    projectId: "eventmanagenment",
-    storageBucket: "eventmanagenment.firebasestorage.app",
-    messagingSenderId: "1092282726690",
-    appId: "1:1092282726690:web:73f98cb8fa9f3e6ef90d7b",
-    measurementId: "G-LF1812PC3D"
-  };
+const firebaseConfig = 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
